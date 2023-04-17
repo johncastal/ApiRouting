7a37169de4b765a153bf54421433ec0b6c467f1a
JOHN FREDY CASTANEDA LONDONO <john.fredy.castaneda@chec.com.co>
2023-04-17 13:39:49 -0500

    Update comments

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ def home():
 
 
 @app.get("/{datos_in}")
-async def read_item(datos_in:str, n_clusters:int=1,seed:int=None,dist_forma:str='Haversine',algoritmo:str='Genetico',ident:str='prueba',key_googlemaps: Union[str, None] = None):
-    datos_in = (datos_in.split(";")) #se transforma a lista
+async def read_item(datos_in:str, n_clusters:int=1,seed:int=None,dist_type:str='Haversine',algorithm:str='Genetico',ident:str='prueba',key_googlemaps: Union[str, None] = None):
+    datos_in = (datos_in.split(";"))
     data_base = {"coordenadas": datos_in, "parametros" : {"n_clusters":n_clusters,"seed":seed}}
     df = pd.DataFrame(data=data_base["coordenadas"],columns=["coordenadas"])
     df = pd.DataFrame(df.coordenadas.str.split(',').tolist(),columns = ['codigo','longitud','latitud'])
     df.longitud =  df.longitud.astype(float)
     df.latitud =  df.latitud.astype(float)
-    #Clusterizacion
+    #Clusterization
     grupos,sede_grupos,df_sedes = clusterizacion(df,n_clusters,seed)
     coor_sedes = df_sedes.to_numpy()
     coor_sedes = coor_sedes[:,1:3]
-    #Generacion de rutas
-    Resumen_df,df_plot,df_rutas = routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_forma,algoritmo,ident,key_googlemaps)
+    #Creation routes
+    Resumen_df,df_plot,df_rutas = routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_type,algorithm,ident,key_googlemaps)
 
     Resumen_df_json = Resumen_df.to_dict(orient='dict')
     df_plot_json =  df_plot.to_dict(orient='dict')
