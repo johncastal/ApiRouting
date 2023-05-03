28ffd2a11c72a4955e84d281722a070d2f430b52
JOHN FREDY CASTANEDA LONDONO <john.fredy.castaneda@chec.com.co>
2023-05-03 10:25:31 -0500

    Update to include info in output related to hightlights.

--- ruteo/get_routing.py
+++ ruteo/get_routing.py
@@ -7,6 +7,7 @@ from ruteo.Instancia import inst
 from ruteo.solvers import solvers
 from ruteo.Funcion_Obj import Funcion_obj
 from ruteo.Get_incum import get_incum
+#from ruteo.cluster_rest import cluster_rest
 #from ruteo.Progress import progress
 
 
@@ -33,11 +34,24 @@ def routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_forma,algoritmo,o
     #progreso.set_Iteraciones(0)
     #progreso.set_Cantidad_distancias(0)
 
+   
+    sede_grupos = sede_grupos[np.lexsort((sede_grupos[:, 2], sede_grupos[:, 1]))]
+    unique_first_col = np.unique(sede_grupos[:, 1], axis=0)
+
+    # Create a new array with unique first column values
+    sede_cluster = np.zeros((unique_first_col.shape[0], sede_grupos.shape[1]))
+    
+    # Iterate over unique values and add to new array
+    for i, val in enumerate(unique_first_col):
+        sede_cluster[i,:] = sede_grupos[sede_grupos[:, 1] == val][0,:]
+
+    #print("sedes:\n", sede_cluster, "\n")
+
     for i in range(n_clusters):
 
         grupo = i # seleccionar cluster
-        sede_grupo_k = sede_grupos[sede_grupos[:,1]==grupo]
-        sede_grupo_k = sede_grupo_k[sede_grupo_k[:, 2].argsort()] # ordena de menor a mayor las distancias de sedes a cluster (grupo) 
+        sede_grupo_k = sede_cluster[sede_cluster[:,1]==grupo]
+        #sede_grupo_k = sede_grupo_k[sede_grupo_k[:, 2].argsort()] # ordena de menor a mayor las distancias de sedes a cluster (grupo) 
         sede_grupo_k = int(sede_grupo_k[0,0]) # selecciona la sede como el primer elemento. 
         coor_sede_k = coor_sedes[sede_grupo_k,:] # se obtienen las coordenadas de la sede
         grupo_k = np.zeros((np.shape(grupos)[0]+1,np.shape(grupos)[1]))
@@ -46,19 +60,20 @@ def routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_forma,algoritmo,o
         grupo_k[0,2:4] = coor_sede_k
         grupo_k[1:,:] = grupos
         grupo_k = grupo_k[grupo_k[:,0] == grupo]
-    
+        
+        #print("grupo_k:\n",pd.DataFrame(grupo_k))
+        #cluster_rest(grupo_k,sede_cluster,20)
+
         #progreso.set_Iteraciones((i)*100/n_clusters)
         #progreso.set_Cantidad_distancias((np.shape(grupo_k)[0])**2)
 
         #print('---cluster número: ',i, ' ---')
         #print('numero de distancias a obtener: ', (np.shape(grupo_k)[0])**2)
 
-        
         #dist_forma='haversine'
         dist = inst(grupo_k,dist_forma,key_googlemaps,objective) #tiempo o distancia (haversine, google) - Distancias para solver principal
         genetico_main = solvers(dist,seed,algoritmo) #se instancia la clase para el solver principal #################
         
-
         if dist_forma == 'Haversine':
             dist2 = dist.copy()
             VecinoCercano = solvers(dist2,seed,'VecinoCercano') # Solver para referencia
@@ -91,7 +106,19 @@ def routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_forma,algoritmo,o
             Resumen_out[i,3] = round((Incum_ref - Incumbente),2)
             Resumen_out[i,4] = round((Incum_ref - Incumbente)*100/Incumbente,2)
             #Resumen_out[i,5] = round((Incum_ref - Incumbente2)*100/Incumbente2,2)
-       
+        
+        # To get highlights
+        if objective == "Distance":
+            highlights = {"Total_incumbent": "{:.2f}".format(np.sum(Resumen_out[:, 2])) + " km",  
+                          "Total_saves" :  "{:.2f}".format(np.sum(Resumen_out[:, 3])) + " km",
+                          "Mean_distance_cluster" : "{:.2f}".format(np.mean(sede_cluster[:,2])) + " km"
+                          }
+        elif objective == "Time":
+            highlights = {"Total_incumbent": "{:.2f}".format(np.sum(Resumen_out[:, 2])) + " Hours",  
+                          "Total_saves" :  "{:.2f}".format(np.sum(Resumen_out[:, 3])) + " Hours",
+                          "Mean_distance_cluster" : "{:.2f}".format(np.mean(sede_cluster[:,2])) + " Hours"
+                          }
+
         sol_incumbete_real,orden_cuentas = get_incum(Sol_Incumbente,grupo_k)
         #print(rutas)
         #print('Ruta: ', sol_incumbete_real)
@@ -124,10 +151,17 @@ def routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_forma,algoritmo,o
         #print('---Resumen resultados---\n')
         #print(Resumen_df.to_markdown())
     elif dist_forma =='Google':
-        Resumen_df = pd.DataFrame(data=Resumen_out,columns=[
-            'Route','ref_cost(km)', 'Google_cost(km-H)', 'Google_saves(km-H)',
-            'Google_saves(%)'
-            ])
+        if objective == "Distance":
+            Resumen_df = pd.DataFrame(data=Resumen_out,columns=[
+                'Route','ref_cost(km)', 'Google_cost(km)', 'Google_saves(km)',
+                'Google_saves(%)'
+                ])
+        elif objective == "Time":
+            Resumen_df = pd.DataFrame(data=Resumen_out,columns=[
+                'Route','ref_cost(H)', 'Google_cost(H)', 'Google_saves(H)',
+                'Google_saves(%)'
+                ])
+
         #print('---Resumen resultados---\n')
         #print(Resumen_df.to_markdown())
 
@@ -154,4 +188,4 @@ def routing(grupos,n_clusters,sede_grupos,coor_sedes,seed,dist_forma,algoritmo,o
     #progreso.set_Iteraciones(100)
     #progreso.set_Cantidad_distancias(0)
 
-    return Resumen_df,df_plot,df_rutas
+    return Resumen_df,df_plot,df_rutas,highlights
